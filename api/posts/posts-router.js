7ac55488ebf3eff0105f4c61615851bdd423f726
Taras Shulhan <taras.shulhan@gmail.com>
2021-12-01 03:16:07 +0200

    got a few

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,58 @@
-// implement your posts router here
+const express = require('express') // commonjs
+const Post = require('./posts-model')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Post.find(req.query)
+      .then(posts => {
+        res.status(200).json(posts);
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          message: 'Error retrieving the posts',
+        });
+      });
+  });
+  
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (post) {
+          res.status(200).json(post);
+        } else {
+          res.status(404).json({ 
+              message: "The post with the specified ID does not exist"
+            });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          message: "The post information could not be retrieved",
+        });
+      });
+  });
+  
+  router.post('/', (req, res) => {
+    if(!req.body.title || !req.body.contents){
+        res.status(400).json({message: "Please provide title and contents for the post"})
+    }else{
+        Post.insert(req.body) 
+        .then(post => {
+            let newPost 
+            Post.findById(post).then(post =>{
+                newPost = post
+            })
+            res.status(201).json(newPost)
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+            message: "There was an error while saving the post to the database",
+            });
+        });
+    }
+  });
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,2 +1,22 @@
-// implement your server here
-// require your posts router and connect it here
+const express = require('express');
+const postsRouter = require('./posts/posts-router');
+
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/posts', postsRouter);
+
+
+// OTHER ENDPOINTS
+// OTHER ENDPOINTS
+// OTHER ENDPOINTS
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Shelter API</h>
+    <p>Welcome to the Shelter API</p>
+  `);
+});
+
+module.exports = server;
